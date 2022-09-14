b3b26593116d0f1a8e606cfee680ddf454da49ea
ueki-yuto <ueki-yuto@dmm.com>
2022-09-14 10:13:46 +0900

    コメント追加

--- main.go
+++ main.go
@@ -24,18 +24,20 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
-			err != http.ErrServerClosed {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed { // ErrServerClosedは正常にシャットダウンされたことを示す
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
 		return nil
 	})
 
+	// キャンセルされるまでここでブロックされ、cancelされたらWebサーバをGraceful Shutdownする
+	// （ただ、キャンセルするような処理を実装していないので、テスト以外では以下は実行されない）
 	<-ctx.Done()
 	if err := s.Shutdown(context.Background()); err != nil {
 		log.Printf("failed to shutdown: %+v", err)
 	}
 
+	// Go関数で実行した関数の戻り値(err)を返す
 	return eg.Wait()
 }
