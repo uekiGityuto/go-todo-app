8e82ea32c6c3ffbacb7b9c8b16296fb26b3d6c0d
ueki-yuto <ueki-yuto@dmm.com>
2022-09-18 23:35:28 +0900

    HTTPサーバー起動手順とルーティング定義を分離

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/uekiGityuto/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 func main() {
@@ -22,9 +17,6 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -36,29 +28,7 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second) // Graceful Shutdown動作確認用
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed { // ErrServerClosedは正常にシャットダウンされたことを示す
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// SIGINT or SIGTERM を受信するまでここでブロック
-	<-ctx.Done()
-	// Graceful Shutdownする
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// Go関数で実行した関数の戻り値(err)を返す
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
--- server_test.go
+++ server_test.go
@@ -11,16 +11,19 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	t.Skip("リファクタリング中")
-
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen prot %v", err)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	in := "message"
